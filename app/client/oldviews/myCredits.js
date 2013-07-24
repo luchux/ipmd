95441301926706684ae144f97902d0b94905260d
luchux <lucianoguasco@gmail.com>
2013-07-25 00:46:41 +1000

    deleted bunches of shit

--- app/client/oldviews/myCredits.js
+++
@@ -1,7 +0,0 @@
-Template.myCredits.helpers({
-   credits: function (user){
-    if (typeof user.userId === 'undefined')
-     return Debts.find({ creditor: Meteor.userId() });
-    return Debts.find({ creditor: Meteor.userId(), debtor:user });
-   }
-});
--- app/client/oldviews/myDebts.js
+++
@@ -1,7 +0,0 @@
-Template.myDebts.helpers({
-   debts: function (user){
-    if (typeof user.userId === 'undefined')
-      return Debts.find({ debtor: Meteor.userId() });
-    return Debts.find({ debtor: Meteor.userId(), creditor: user });
-   }
-});
