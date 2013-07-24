5cc84cacfe1b02f983afc367d50b3541deef0c6f
luchux <lucianoguasco@gmail.com>
2013-07-25 01:02:37 +1000

    added totalDebts totalCredits to show amount

--- app/client/views/debts.js
+++ app/client/views/debts.js
@@ -1,3 +1,5 @@
+/*** Template myDebts ***/
+
 Template.myDebts.helpers({
    debts: function (user){
     if (typeof user.userId === 'undefined')
@@ -6,9 +8,22 @@ Template.myDebts.helpers({
    }
 });
 
+/*** Template myCredits ***/
+
 Template.myCredits.helpers({
     credits: function() {
         return Debts.find({ 'creditor': Meteor.userId()});
     }
 });
 
+/*** Template mainDebts ***/
+
+Template.mainDebts.helpers({
+    totalDebts: function() {
+        return Debts.find({ debtor: Meteor.userId(), status:'waiting' }).count()
+    },
+
+    totalCredits: function() {
+        return Debts.find({ debtor: Meteor.userId(), status:'waiting' }).count()
+    }
+});
